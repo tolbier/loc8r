b72c4d337933c7ad2ba7218dbc056b7b3fbcbed1
tolbier <ltoyos@gmail.com>
2016-12-01 22:47:46 +0100

    adding model

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -5,7 +5,7 @@ var ctrlLocations = require('../controllers/locations');
 var ctrlOthers = require('../controllers/others');
 
 /* GET home page. */
-router.get('/',                     ctrlLocations.homeList);
+router.get('/',                     ctrlLocations.homelist);
 router.get('/location',             ctrlLocations.locationInfo);
 router.get('/location/review/new',  ctrlLocations.addReview);
 
