c753920ce97f37f03d6fca31ad87eac9c26753d5
tolbier <ltoyos@gmail.com>
2016-12-04 00:15:50 +0100

    API REST funcionando

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-require('./app_server/models/db');
+require('./app_api/models/db');
 
-var index = require('./app_server/routes/index');
-var users = require('./app_server/routes/users');
+var routes = require('./app_server/routes/index');
+var routesApi = require('./app_api/routes/index');
+///var users = require('./app_server/routes/users');
 
 var app = express();
 
@@ -23,8 +24,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', routes);
+app.use('/api', routesApi);
+//app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
