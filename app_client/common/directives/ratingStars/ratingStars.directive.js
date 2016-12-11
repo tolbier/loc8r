961b1a02b67fee597977182d1337bba394dd2ad1
tolbier <ltoyos@gmail.com>
2016-12-11 16:11:10 +0100

    Funcionando location con AngularJS

---
+++ app_client/common/directives/ratingStars/ratingStars.directive.js
@@ -0,0 +1,13 @@
+angular
+    .module('loc8rApp')
+    .directive('ratingStars', ratingStars);
+
+function ratingStars() {
+    return {
+        restrict: 'EA',
+        scope: {
+            thisRating: '=rating'
+        },
+        templateUrl: '/common/directives/ratingStars/ratingStars.template.html'
+    };
+}
