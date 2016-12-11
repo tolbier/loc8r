0c0200ac136481c2aca92d14e26b5eb6b16e6bfc
tolbier <ltoyos@gmail.com>
2016-12-11 18:16:22 +0100

    uglified

--- app_client/common/directives/ratingStars/ratingStars.directive.js
+++ app_client/common/directives/ratingStars/ratingStars.directive.js
@@ -1,13 +1,15 @@
-angular
-    .module('loc8rApp')
-    .directive('ratingStars', ratingStars);
+(function() {
+    angular
+        .module('loc8rApp')
+        .directive('ratingStars', ratingStars);
 
-function ratingStars() {
-    return {
-        restrict: 'EA',
-        scope: {
-            thisRating: '=rating'
-        },
-        templateUrl: '/common/directives/ratingStars/ratingStars.template.html'
-    };
-}
+    function ratingStars() {
+        return {
+            restrict: 'EA',
+            scope: {
+                thisRating: '=rating'
+            },
+            templateUrl: '/common/directives/ratingStars/ratingStars.template.html'
+        };
+    }
+})();
--- app_client/common/services/loc8rData.service.js
+++ app_client/common/services/loc8rData.service.js
@@ -1,13 +1,16 @@
-angular
-    .module('loc8rApp')
-    .service('loc8rData', loc8rData);
-
-function loc8rData($http) {
-    var locationByCoords = function(lat, lng) {
-        return $http.get('/api/locations?lng=' + lng + '&lat=' + lat +
-            '&maxDistance=1200');
-    };
-    return {
-        locationByCoords: locationByCoords
-    };
-}
+(function() {
+    angular
+        .module('loc8rApp')
+        .service('loc8rData', loc8rData);
+        
+    loc8rData.$inject = ['$http'];
+    function loc8rData($http) {
+        var locationByCoords = function(lat, lng) {
+            return $http.get('/api/locations?lng=' + lng + '&lat=' + lat +
+                '&maxDistance=1200');
+        };
+        return {
+            locationByCoords: locationByCoords
+        };
+    }
+})();
