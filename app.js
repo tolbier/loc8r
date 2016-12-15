6e5bf3a60fb92955a31061ebd6b83ec57af245da
tolbier <ltoyos@gmail.com>
2016-12-15 21:56:07 +0100

    quitados app_server y archivos no usados

--- app.js
+++ app.js
@@ -15,15 +15,15 @@ var passport = require('passport');
 require('./app_api/models/db');
 require('./app_api/config/passport');
 
-var routes = require('./app_server/routes/index');
+//var routes = require('./app_server/routes/index');
 var routesApi = require('./app_api/routes/index');
 ///var users = require('./app_server/routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'app_server', 'views'));
-app.set('view engine', 'jade');
+//app.set('views', path.join(__dirname, 'app_server', 'views'));
+//app.set('view engine', 'jade');
 
 
 var appClientFiles = [
@@ -89,33 +89,35 @@ app.use(function(req, res, next) {
 
 // error handlers
 // Catch unauthorised errors
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   if (err.name === 'UnauthorizedError') {
     res.status(401);
-    res.json({"message" : err.name + ": " + err.message});
+    res.json({
+      "message": err.name + ": " + err.message
+    });
   }
 });
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 module.exports = app;
